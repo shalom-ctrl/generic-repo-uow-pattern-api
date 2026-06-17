93e6a7a74bcf2f97e1b0110a5dce559d2deb2b5d
Shalom Achikasim <shalomachims@gmail.com>
2026-06-17 12:20:41 +0100

    Add ProductWithUOWController

---
+++ generic-repo-uow-pattern/Controllers/ProductWithUOWController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace generic_repo_uow_pattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductWithUOWController : ControllerBase
+    {
+    }
+}
