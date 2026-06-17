6018a0411ee91cf308d1d9a6968333635268f5f5
Shalom Achikasim <shalomachims@gmail.com>
2026-06-17 10:27:59 +0100

    Initial Commit

---
+++ generic-repo-uow-pattern/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace generic_repo_uow_pattern
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
